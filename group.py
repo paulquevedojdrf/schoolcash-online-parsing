b7bbb90539d865e1c83c4995131c3618ceca6f79
Paul Quevedo <PaulQuevedo@jdrfelectromag.com>
2025-06-18 21:56:40 -0400

    add custom ordering of teacher groups

--- group.py
+++ group.py
@@ -9,6 +9,10 @@ from pathlib import Path
 from collections import defaultdict
 from argparse import ArgumentParser
 from pprint import pprint
+try:
+    from teacher_order import teacher_order
+except ImportError:
+    teacher_order = []
 
 def make_empty_entry(template):
     empty = {}
@@ -21,10 +25,19 @@ def main(args):
         reader = csv.DictReader(f)
         data = [row for row in reader]
 
+    teacher_order_map = {name: i for i, name in enumerate(teacher_order)}
+
     # Filter out entries we dont care about
     data = [x for x in data if x['Teacher'] not in ("",None) and x['First Name'] not in ("",None)]
     # Sort by teacher, then by first name and finally by last name
-    data = sorted(data, key=lambda x: (x['Teacher'], x['First Name'], x['Last Name']))
+    data = sorted(
+        data,
+        key=lambda x: (
+            teacher_order_map.get(x['Teacher'], len(teacher_order)),
+            x['First Name'],
+            x['Last Name']
+        )
+    )
 
     # Split the list into a list per class (keyed on teacher name)
     by_teacher = defaultdict(list)
