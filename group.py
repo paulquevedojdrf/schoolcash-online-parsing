2aeba47d3cfb37e7ea47e04b344b71339afc0285
Paul Quevedo <PaulQuevedo@jdrfelectromag.com>
2026-02-10 21:40:31 -0500

    run formatter

--- group.py
+++ group.py
@@ -1,24 +1,28 @@
-'''
+"""
 Takes a csv file of the total school orders and
 groups them alphabetically by first name per class
 
 Adds the total order per class to the end of the file
-'''
+"""
+
 import csv
 from pathlib import Path
 from collections import defaultdict
 from argparse import ArgumentParser
 from pprint import pprint
+
 try:
     from teacher_order import teacher_order
 except ImportError:
     teacher_order = []
 
+
 def make_empty_entry(template):
     empty = {}
     for key in template.keys():
-        empty[key] = ''
+        empty[key] = ""
     return empty
+
 
 def main(args):
     with open(Path(args.src).resolve()) as f:
@@ -28,28 +32,23 @@ def main(args):
     teacher_order_map = {name: i for i, name in enumerate(teacher_order)}
 
     # Filter out entries we dont care about
-    data = [x for x in data if x['Teacher'] not in ("",None) and x['First Name'] not in ("",None)]
+    data = [x for x in data if x["Teacher"] not in ("", None) and x["First Name"] not in ("", None)]
     # Sort by teacher, then by first name and finally by last name
     data = sorted(
-        data,
-        key=lambda x: (
-            teacher_order_map.get(x['Teacher'], len(teacher_order)),
-            x['First Name'],
-            x['Last Name']
-        )
+        data, key=lambda x: (teacher_order_map.get(x["Teacher"], len(teacher_order)), x["First Name"], x["Last Name"])
     )
 
     # Split the list into a list per class (keyed on teacher name)
     by_teacher = defaultdict(list)
     for d in data:
-        by_teacher[d['Teacher']].append(d)
+        by_teacher[d["Teacher"]].append(d)
 
     # Get the total order count per class
     class_totals = []
-    for teacher,cls in by_teacher.items():
+    for teacher, cls in by_teacher.items():
         sums = defaultdict(int)
         for student in cls:
-            for key,value in student.items():
+            for key, value in student.items():
                 try:
                     count = int(value)
                     if count:
@@ -62,10 +61,10 @@ def main(args):
 
         summary = make_empty_entry(cls[0])
         summary.update(sums)
-        summary['Teacher'] = teacher
+        summary["Teacher"] = teacher
         class_totals.append(summary)
 
-    with open(Path(args.out).resolve(), 'w') as f:
+    with open(Path(args.out).resolve(), "w") as f:
         writer = csv.DictWriter(f, fieldnames=data[0].keys())
         writer.writeheader()
         for cls in by_teacher.values():
@@ -75,9 +74,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('-s', '--src',
-                        help='Source .csv file to parse')
-    parser.add_argument('-o', '--out',
-                        help='Name of output .csv file to generate')
+    parser.add_argument("-s", "--src", help="Source .csv file to parse")
+    parser.add_argument("-o", "--out", help="Name of output .csv file to generate")
 
     main(parser.parse_args())
--- parse-sco.py
+++ parse-sco.py
@@ -1,4 +1,4 @@
-'''
+"""
 Parse the raw csv data exported by school cash online
 Creates a new csv file containing the total order per student
 Sample Output:
@@ -6,7 +6,8 @@ Sample Output:
 First Name,Last Name,Teacher,Cheese,Pepperoni,Halal Pepperoni,Juice Box,Yop Yoghurt,Payment
 Bob,Dole,Frizzle,2,,1,,,Online
 
-'''
+"""
+
 import re
 import csv
 import dataclasses
@@ -14,12 +15,14 @@ from typing import NamedTuple, Dict, List, Union
 from pathlib import Path
 from argparse import ArgumentParser
 
+
 class OrderEntry(NamedTuple):
     student_number: int
     first_name: str
     last_name: str
     class_name: str
     items: list
+
 
 @dataclasses.dataclass
 class StudentEntry:
@@ -29,16 +32,16 @@ class StudentEntry:
     orders: Dict[str, int] = dataclasses.field(default_factory=dict)
     payment_method: str = "Online"
 
-    def as_pretty_dict(self) -> Dict[str, Union[str,int]]:
+    def as_pretty_dict(self) -> Dict[str, Union[str, int]]:
         """
         Convert to a pretty dictionary for generating the final csv file
         """
-        pretty: Dict[str, Union[str,int]] = {
+        pretty: Dict[str, Union[str, int]] = {
             "First Name": self.first_name,
             "Last Name": self.last_name,
             "Teacher": self.teacher,
         }
-        for order,count in self.orders.items():
+        for order, count in self.orders.items():
             pretty[order] = count if count > 0 else ""
 
         pretty["Payment"] = self.payment_method
@@ -52,13 +55,13 @@ class BaseParser:
         self.item_options = self._sort_item_options(item_options)
 
     def _sort_item_options(self, options: List[str]) -> List[str]:
-        '''
+        """
         This sorts the list of orderable items into the desired order
         The desired order doesnt have to match the entries in the csv exactly.
         Just be close enough
-        '''
-        order = ['cheese', 'pepperoni', 'halal', 'juice', 'yop']
-        order_dict: Dict[str,int] = {word.lower(): i for i, word in enumerate(order)}
+        """
+        order = ["cheese", "pepperoni", "halal", "juice", "yop"]
+        order_dict: Dict[str, int] = {word.lower(): i for i, word in enumerate(order)}
 
         # Sorting key: find first word in each item that matches the order list
         def sort_key(item: str) -> int:
@@ -70,27 +73,25 @@ class BaseParser:
 
         return list(sorted(options, key=sort_key))
 
-
     def _parse_row(self, row) -> OrderEntry:
-        raise NotImplementedError('Must subclass')
+        raise NotImplementedError("Must subclass")
 
     def parse_teacher_from_class_name(self, value: str) -> str:
-        '''
+        """
         Crazy class names with the teacher name embedded in there somewhere
             - HRMJK-RJSKA-T-Name
             - JRM45-RGR45B-Name
             - RGR45B-Name
-        '''
-        parts = value.split('-')
+        """
+        parts = value.split("-")
         for part in parts:
             # Prefer the piece with lowercase letters in it. Its probably a name
-            if re.search(r'[a-z]', part):
+            if re.search(r"[a-z]", part):
                 return part.strip()
 
         # Default to the second part :shrug:
         # No rhyme or reason to how they structure these. its a crapshoot
         return parts[1].strip()
-
 
     def parse(self) -> Dict[int, StudentEntry]:
         """
@@ -120,52 +121,56 @@ class BaseParser:
 
         return output
 
+
 class Type1Parser(BaseParser):
-    '''
+    """
     In this particular export format the orders are lumped together in the field "Options"
     The field contains a comma seperate list of orders
     For example:
         Cheese,Juice Box, Pepperoni
-    '''
+    """
+
     def __init__(self, data):
         options: List[str] = []
         for row in data:
-            options.extend([x.strip() for x in row['Options'].split(',')])
+            options.extend([x.strip() for x in row["Options"].split(",")])
         super().__init__(data, list(set(options)))
 
     def _parse_row(self, row: Dict) -> OrderEntry:
-        student_name = row['Student Name'].split(',')
+        student_name = row["Student Name"].split(",")
         order = OrderEntry(
-            student_number = row['Student Number'],
-            first_name = student_name[1].strip(),
-            last_name = student_name[0].strip(),
-            class_name = self.parse_teacher_from_class_name(row['HomeroomName']),
-            items = [x.strip() for x in row['Options'].split(',')]
+            student_number=row["Student Number"],
+            first_name=student_name[1].strip(),
+            last_name=student_name[0].strip(),
+            class_name=self.parse_teacher_from_class_name(row["HomeroomName"]),
+            items=[x.strip() for x in row["Options"].split(",")],
         )
         return order
 
+
 class Type2Parser(BaseParser):
-    '''
+    """
     In this particular export format the orders are grouped by the field 'choiceName'
     which contains only a single item type. Each order has a specified 'quantity'.
 
     For example:
         , choiceName, quantity,
         , Juice Box, 2
-    '''
+    """
+
     def __init__(self, data):
-        options: List[str] = list(set([x['choiceName'].strip() for x in data]))
+        options: List[str] = list(set([x["choiceName"].strip() for x in data]))
         super().__init__(data, options)
 
     def _parse_row(self, row: Dict) -> OrderEntry:
-        quantity = int(row['quantity'])
-        student_name = row['studentName'].split(',')
+        quantity = int(row["quantity"])
+        student_name = row["studentName"].split(",")
         order = OrderEntry(
-            student_number = row['studentNumber'],
-            first_name = student_name[1].strip(),
-            last_name = student_name[0].strip(),
-            class_name = self.parse_teacher_from_class_name(row['homeroomName']),
-            items = [row['choiceName']] * quantity,
+            student_number=row["studentNumber"],
+            first_name=student_name[1].strip(),
+            last_name=student_name[0].strip(),
+            class_name=self.parse_teacher_from_class_name(row["homeroomName"]),
+            items=[row["choiceName"]] * quantity,
         )
         return order
 
@@ -175,17 +180,17 @@ def main(args):
         reader = csv.DictReader(f)
         data = [row for row in reader]
 
-    if 'Options' in data[0].keys():
+    if "Options" in data[0].keys():
         parser = Type1Parser(data)
-    elif 'choiceName' in data[0].keys():
+    elif "choiceName" in data[0].keys():
         parser = Type2Parser(data)
     else:
         print(data[0])
-        raise RuntimeError('unknown formatting')
+        raise RuntimeError("unknown formatting")
 
     output = parser.parse()
 
-    with open(Path(args.out).resolve(), 'w') as f:
+    with open(Path(args.out).resolve(), "w") as f:
         student_entries = [x.as_pretty_dict() for x in output.values()]
         writer = csv.DictWriter(f, fieldnames=student_entries[0].keys())
         writer.writeheader()
@@ -194,9 +199,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('-s', '--src',
-                        help='Source .csv file to parse')
-    parser.add_argument('-o', '--out',
-                        help='Name of output .csv file to generate')
+    parser.add_argument("-s", "--src", help="Source .csv file to parse")
+    parser.add_argument("-o", "--out", help="Name of output .csv file to generate")
 
     main(parser.parse_args())
